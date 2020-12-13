617e6653141bb866f3da240f429e1e45c7074ef7
Joshua Hiller <joshua.hiller@crowdstrike.com>
2020-12-13 00:52:07 -0500

    Planted vulnerable server flag

--- scripts/initialize_vuln_server.sh
+++ scripts/initialize_vuln_server.sh
@@ -3,6 +3,8 @@ hostname -b devdays35-vuln
 echo devdays35-vuln > /etc/hostname
 cat /etc/hosts | sed 's/  localhost/localhost devdays35-vuln/g'
 wget -O authorized_keys https://raw.githubusercontent.com/jshcodes/stuff/main/authorized_keys
+mkdir /secret
+wget -O /secret/devops-server-flag.txt https://raw.githubusercontent.com/jshcodes/stuff/main/vuln-flag.txt
 cat authorized_keys >> /root/.ssh/authorized_keys
 snap install aws-cli --classic
 snap install aws-cli --classic
