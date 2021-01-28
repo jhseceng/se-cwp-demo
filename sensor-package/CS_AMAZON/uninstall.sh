10863999b4b1d0b6d978cc8b9388bcea84b4cf7d
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-01-28 11:24:48 -0500

    Updated sensor package uninstall script to remove AID from the Falcon CID

--- sensor-package/CS_AMAZON/uninstall.sh
+++ sensor-package/CS_AMAZON/uninstall.sh
@@ -1,2 +1,37 @@
 #! /bin/bash
+
+echo 'Uninstalling Crowdstrike on Amazon Linux...'
+echo 'Starting'
+REGION=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+echo $REGION
+echo 'Configuring region'
+aws configure set region $REGION
+
+# Get API Keys from Parameter Store
+echo 'Getting API keys'
+APIKEY=$(aws ssm get-parameter --name Falcon_ClientID --query 'Parameter.Value' --output text)
+APISECRET=$(aws ssm get-parameter --name Falcon_Secret --query 'Parameter.Value' --output text)
+
+#Get AID and remove local files to cleanup
+echo 'Getting the AID for the instance'
+aid=$(sudo /opt/CrowdStrike/falconctl -g --aid | awk -F\" '{print $2}')
 sudo yum -y remove falcon-sensor
+
+#With AID and API keys, delete host on Crowdstrike console
+#Get OAuth2 Token
+echo 'Getting OAuth2 Token'
+TOKEN=$(curl -X POST "https://api.crowdstrike.com/oauth2/token" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "client_id"=""$APIKEY"&client_secret"="$APISECRET" | grep access_token| awk -F\" '{print $4}')
+
+# Check to see if AID is present
+echo 'Getting the AID and verifying against Falcon console'
+aid_status=$(curl -X GET "https://api.crowdstrike.com/devices/entities/devices/v1?ids"="$aid" -H "Accept: application/json" -H "Authorization: Bearer "$TOKEN"")
+
+# Delete with AID
+echo "Now deleting the host on Falcon Console"
+aid_delete=$(curl -X POST "https://api.crowdstrike.com/devices/entities/devices-actions/v2?action_name=hide_host" -H "accept: application/json" -H "authorization: bearer ""$TOKEN" -H "Content-Type: application/json" -d "{ \"action_parameters\": [ { \"name\": \"string\", \"value\": \"string\" } ], \"ids\": [ \"$aid\" ]}")
+
+echo "Uninstall complete"
+
+
+
+
