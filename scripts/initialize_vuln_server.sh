ee7320c121395ac3ff45402d266d485982e458bf
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-02-17 14:03:56 -0500

    Amazon Linux 2 adjustments

--- scripts/initialize_vuln_server.sh
+++ scripts/initialize_vuln_server.sh
@@ -6,5 +6,7 @@ wget -O authorized_keys https://raw.githubusercontent.com/jshcodes/stuff/main/au
 mkdir /secret
 wget -O /secret/devops-server-flag.txt https://raw.githubusercontent.com/jshcodes/stuff/main/vuln-flag.txt
 cat authorized_keys >> /root/.ssh/authorized_keys
-snap install aws-cli --classic
-snap install aws-cli --classic
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm awscliv2.zip
